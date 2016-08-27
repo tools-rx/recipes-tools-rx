3d4241643bc861c7e32dddb959f027d6155effbe
Dave Baskin <dfbaskin@gmail.com>
2016-08-27 14:55:33 -0400

    Updating watch files example.

--- docs/globs/watch-files/demo.js
+++ docs/globs/watch-files/demo.js
@@ -1,15 +1,21 @@
 
 import path from 'path'
+import fs from 'fs'
 import {Observable} from 'rxjs'
 import {watchRx} from 'watch-rx'
 
-let fromPath = path.join(__dirname, 'files')
+const writeFileRx = Observable.bindNodeCallback(fs.writeFile)
+const renameRx = Observable.bindNodeCallback(fs.rename)
+const unlinkRx = Observable.bindNodeCallback(fs.unlink)
 
+const fromPath = path.join(__dirname, 'files')
+
+// Watch for file changes
 watchRx('**/*', { cwd: fromPath })
   .takeUntil(Observable.timer(3000))
   .subscribe({
     next (file) {
-      console.log('Found file:', file.fullname)
+      console.log(`File[${file.event}] : ${file.name}`)
     },
     error (err) {
       console.error('Error:', err)
@@ -18,3 +24,13 @@ watchRx('**/*', { cwd: fromPath })
       console.log('End of file list.')
     }
   })
+
+// Then make some changes to the files
+Observable
+  .timer(1000)
+  .concat(writeFileRx(path.join(fromPath, 'a', 'two.txt'), 'updated'))
+  .concat(unlinkRx(path.join(fromPath, 'b', 'three.txt')))
+  .concat(renameRx(path.join(fromPath, 'c', 'five.txt'), path.join(fromPath, 'c', 'five-renamed.txt')))
+  .concat(writeFileRx(path.join(fromPath, 'c', 'six.txt'), 'new file'))
+  .catch((err) => console.error(err))
+  .subscribe()
